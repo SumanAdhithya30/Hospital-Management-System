6b5aaf2106267e45a1dea352c67c474747691d0e
SumanAdhithya30 <sumanadhithya701@gmail.com>
2024-12-23 13:46:16 +0530

    Added backend files excluding frontend

---
+++ models/appointment.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema({
+  patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true },
+  doctorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
+  date: { type: Date, required: true },
+  time: { type: String, required: true },
+  purpose: { type: String, required: true }
+});
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+module.exports = Appointment;
